2db53de98cb29803115457465de1e3471e2a2968
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2015-07-30 07:31:42 +0200

    models added

---
+++ model/LONG.php
@@ -0,0 +1,12 @@
+<h4>HEADLINE</h4>
+<div class="row">
+<div class="large-12 columns">
+	<img src="IMAGEPATH"/> 
+</div>
+</div>
+<div class="row">
+	<div class="large-12 columns">
+		<p>TEXT OF REPORT
+		</p>
+	</div>
+</div>
---
+++ model/SHORT.php
@@ -0,0 +1,8 @@
+<h4>HEADLINE</h4>
+<div class="row">
+<div class="large-12 columns">
+	<img src="IMGPATH"/> 
+</div>
+</div>
+</br>
+
